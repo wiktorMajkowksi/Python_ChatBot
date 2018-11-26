8d1bee6c567cbb05a73d2bb0509056a1bb277b6f
Wiktor Majkowski (majkowsw) <majkowsw@coventry.ac.uk>
2018-11-26 21:50:58 +0000

    Delete ice-cream.py

--- ice-cream.py
+++
@@ -1,9 +0,0 @@
-scoops = ("How many scoops would you like?")
-
-def numScoops(scoops):
-	userInput = input("You: ").lower()
-	return userInput
-
-print(scoops)
-userInput = ("")
-print("I will bring " + numScoops(scoops) + " scoops.")
